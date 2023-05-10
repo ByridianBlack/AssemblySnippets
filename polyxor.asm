8f016b57a3d3fbd48892592d013325c18df536c1
ByridianBlack <pco23@scarletmail.rutgers.edu>
2023-05-10 14:39:21 -0400

    cave kinda annoying

--- polyxor.asm
+++ polyxor.asm
@@ -7,7 +7,7 @@ segment readable executable writeable
 start:
 
 
-    call encryption_stub
+    call test_code
 
 ;--------------------------------------------------------
 ; All encrypted data should be in the following format:
@@ -29,25 +29,137 @@ start:
 ; ( 6 is the header information )
 ;
 ;--------------------------------------------------------
-encrypted:
-    db 0x17,0x00,0x00,0x00,0x55,0x1d,0x64,0x95,0x1d,0x92,0x95,0x69,0x55,0x55,0x55,0x1d,0x64,0xaa,0x1d,0x92,0x92,0x11,0x55,0x55,0x55,0x5a,0x50,0x59
+; encrypted:
+;     db 0x17,0x00,0x00,0x00,0x55,0x1d,0x64,0x95,0x1d,0x92,0x95,0x69,0x55,0x55,0x55,0x1d,0x64,0xaa,0x1d,0x92,0x92,0x11,0x55,0x55,0x55,0x5a,0x50,0x59
 
-encryption_stub:
-    mov rdi, encrypted
+; encryption_stub:
+;     mov rdi, encrypted
 
-    mov ecx, DWORD [rdi] ; DATA SIZE
-    add rdi, 4
+;     mov ecx, DWORD [rdi] ; DATA SIZE
+;     add rdi, 4
     
-    mov al, BYTE [rdi]      ; KEY
+;     mov al, BYTE [rdi]      ; KEY
+;     inc rdi
+
+
+;     xor_routine:
+;         xor BYTE [rdi], al
+
+;         inc rdi
+
+;         loop xor_routine
+
+;     call encrypted + 6
+;     ret
+
+
+
+; --------------------------------------------------------
+; Before was the old version. I think I should 
+; improve it. Encrypting and then decrypting itself
+; makes no sesne and close to impossible.
+; Rather it makes more sense to just have a function
+; decrypt, call, then immediately encrypt
+; the functions in question. This way, there is a method
+; to keep track of all the data. such as changes in keys
+; and such.
+;
+; same format should persist though for encrypted data
+;
+; Points to remember
+; Most see the RWX permissions as odd so I should 
+; configure some type of function which changes the 
+; permissions accordingly to wwhat I need. I have read
+; that first changing it to R-W then RX- and then finally
+; R-W should be enough to avoid any suspicions.
+; --------------------------------------------------------
+
+; encrypted:
+;     db 0x17,0x00,0x00,0x00,0x55,0x1d,0x64,0x95,0x1d,0x92,0x95,0x69,0x55,0x55,0x55,0x1d,0x64,0xaa,0x1d,0x92,0x92,0x11,0x55,0x55,0x55,0x5a,0x50,0x59
+
+; ; xor_procedure:
+; ;     mov rdi, encrypted
+
+; ;     mov ecx, DWORD [rdi] ; DATA SIZE
+; ;     add rdi, 4
+    
+; ;     mov al, BYTE [rdi]      ; KEY
+; ;     inc rdi
+
+
+; ;     xor_routine:
+; ;         xor BYTE [rdi], al
+
+; ;         inc rdi
+
+; ;         loop xor_routine
+
+; ;     call encrypted + 6
+
+; ;     ret
+
+; ; ------------------------------------ 
+; ; Arguments: 
+; ; rdi - address of encrypted_data
+; ; ------------------------------------
+; xor_procedure:
+
+;     push rcx
+;     push rax
+;     push rdi
+
+;     mov ecx, DWORD [rdi]                    ; DATA SIZE
+;     add rdi, 4
+
+;     push ecx
+
+;     mov al, BYTE[rdi]                       ; KEY
+;     inc rdi
+
+;     xor_routine_e:
+;         xor BYTE [rdi], al
+;         inc rdi
+
+;         loop xor_routine_e
+    
+;     pop ecx
+;     pop rdi
+
+;     call rdi+6
+
+;     add rdi, 4
+
+;     add al, 1                               ; Need to update KEY but this time I need to make the value is never 0;
+;     mov BYTE[rdi], al           
+;     inc rdi
+
+;     xor_routine_d:
+;         xor BYTE[rdi], al
+;         inc rdi
+
+;         loop xor_routine_d
+
+;     pop rax
+;     pop rcx
+
+
+test_code:
+
+
+    push rdi
+    push rax
+
+
+    xor rax, rax
+    inc rax
+
+    xor rdi, rdi
     inc rdi
 
+    mov rsi, 0x68732f6e69622f2f
+    mov rdx, 3
 
-    xor_routine:
-        xor BYTE [rdi], al
+    syscall
 
-        inc rdi
-
-        loop xor_routine
-
-    call encrypted + 6
-    ret
+    pop rax
+    pop rdi
